08a779b2d98e5bc56b8ea90025fe8320044ccb2e
JPBotelho <worldoftanksreviewer@gmail.com>
2018-02-22 21:05:32 +0000

    Dump

---
+++ Content/GradientCreator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class GradientCreator : MonoBehaviour {
+
+	public AnimationCurve curve;
+	public int width;
+
+	public bool execute;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (execute)
+		{
+			Execute();
+			execute = false;
+		}	
+	}
+
+	public void Execute ()
+	{
+		Texture2D texture = new Texture2D(width, 1, TextureFormat.ARGB32, false);
+		Color[] pixels = texture.GetPixels();
+
+		float minScaleDivision = 1f/width;
+
+		for (int i = 0; i < pixels.Length; i++)
+		{
+			float samplePosition = minScaleDivision * (i);
+
+			float grayscale = curve.Evaluate(samplePosition);
+			 
+			Color newColor = FromGrayscale(grayscale);
+
+			pixels[i] = newColor;
+		}
+
+		texture.SetPixels(pixels);
+
+		ExportImage(texture);
+	}
+
+	public static void ExportImage(Texture2D texture)
+	{
+		string path = GetImagePath();
+
+		if (!string.IsNullOrEmpty(path))
+		{
+			byte[] bytes = texture.EncodeToPNG();
+			System.IO.File.WriteAllBytes(path, bytes);
+			AssetDatabase.Refresh();
+		}
+	}
+
+	public static string GetImagePath ()
+	{
+		string path = EditorUtility.SaveFilePanel ("Save Image", Application.dataPath, "Fractal", "png");
+
+		return path;
+	}
+
+	public static Color FromGrayscale (float grayscale)
+	{
+		float clamped = grayscale;
+
+		return new Color(clamped, clamped, clamped, 1);
+	}
+}
---
+++ Content/Toon Shader.shader
@@ -0,0 +1,53 @@
+﻿Shader "Custom/Toon Shader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Ramp ("Ramp Texture", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Ramp
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+		sampler2D _Ramp;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		float4 LightingRamp (SurfaceOutput s, half3 lightDir, half atten) 
+		{
+        	float NdotL = dot (s.Normal, lightDir);
+        	float diff = NdotL * 0.5 + 0.5;
+
+        	float3 ramp = tex2D (_Ramp, float2(diff, 1)).rgb; //Samples from the procedural texture
+
+        	float4 c;
+        	c.rgb = s.Albedo * _LightColor0.rgb * ramp * atten;
+
+        	return c;
+    	}
+
+		void surf (Input IN, inout SurfaceOutput o) 
+		{
+			// Albedo comes from a texture tinted by color
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
